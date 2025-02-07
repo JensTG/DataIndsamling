4ff3c6796a38ef0aeb5bf0f41ebdf5e420dea302
Victor Süberkrüb <victorsubskole@gmail.com>
2025-02-07 12:22:50 +0100

    DAL start

--- DAL.cs
+++ DAL.cs
@@ -8,5 +8,40 @@ namespace DataIndsamling
 {
     internal class DAL
     {
-    }
+        static string personPath = @"Persons.txt";
+        static string dataPath = @"Data.txt";
+        static void AddText(string[] Person, string[] Data)
+        {
+            using (StreamWriter swc = File.AppendText(personPath))
+            {
+                swc.WriteLine("{0},{1},{2},{3}", Person[0], Person[1], Person[2], Person[3]);
+                swc.Close();
+            }
+            using (StreamWriter swc = File.AppendText(dataPath))
+            {
+                foreach (string svar in Data)
+                {
+                    swc.WriteLine("{0},{1}");
+                 }
+                swc.Close();
+            }
+
+        }
+
+        static string ReadData(int index)
+        {
+            using (StreamReader sr = File.OpenText(personPath))
+            {
+                string s;
+                List<string> Data = new List<string>();
+                while ((s = sr.ReadLine()) != null)
+                {
+                    string[] subs = s.Split(',');
+                    chart1.Series["Brugere"].Points.AddXY(Convert.ToString(subs[1]), Convert.ToInt16(subs[0]));
+                }
+
+            }
+
+
+        }
 }
